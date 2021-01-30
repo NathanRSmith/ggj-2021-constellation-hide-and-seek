c2b296def0cd79b7475dce4425344792b4dee495
Nathan Smith <nathanrandal@gmail.com>
2021-01-30 14:33:09 -0500

    achieved random stars in sky and highlighting

---
+++ Assets/RandomStarSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Camera-Control/Star Spawner")]
+public class RandomStarSpawner : MonoBehaviour {
+  public GameObject starTemplate;
+  public GameObject starsContainer;
+  public int numStars = 100;
+  public int dist = 500;
+
+  // Start is called before the first frame update
+  void Start() {
+    for (int i = 0; i < numStars; i++) {
+      SpawnRandom();
+    }
+  }
+
+
+  void SpawnRandom() {
+    var rand = Random.onUnitSphere * dist;
+    rand.y = Mathf.Abs(rand.y);
+    Instantiate(starTemplate, rand, new Quaternion(), starsContainer.transform);
+  }
+}
---
+++ Assets/StarAction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarAction : MonoBehaviour {
+  bool highlighted = false;
+  void OnMouseOver() {
+    if (highlighted) return;
+    transform.localScale *= 2;
+    highlighted = true;
+  }
+  void OnMouseExit() {
+    transform.localScale /= 2;
+    highlighted = false;
+  }
+}
